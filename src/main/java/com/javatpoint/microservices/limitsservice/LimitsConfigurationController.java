66f5f10077568f888a96ec91f7939dbe58909f12
Alexandre Franca <basic4@gmail.com>
2021-06-20 16:10:59 +0100

    feat: Configuring profiles for Limit Service

--- src/main/java/com/javatpoint/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/javatpoint/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,9 +1,11 @@
 package com.javatpoint.microservices.limitsservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
 @RestController
 public class LimitsConfigurationController {
 	@Autowired
